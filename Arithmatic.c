7c201652e8dd3cdfc691fed41c5217f91ef7f272
AFMT11 <AFMT11@yahoo.com>
2022-08-18 15:13:28 +0200

    1.0.0

---
+++ Arithmatic.c
@@ -0,0 +1,6 @@
+
+int Sum(int a, int b)
+{
+    int sum  = a + b;
+    return sum;
+}
---
+++ Arithmatic.h
@@ -0,0 +1,6 @@
+#ifndef ARITHMATIC_H_INCLUDED
+#define ARITHMATIC_H_INCLUDED
+
+int Sum(int a, int b);
+
+#endif // ARITHMATIC_H_INCLUDED
